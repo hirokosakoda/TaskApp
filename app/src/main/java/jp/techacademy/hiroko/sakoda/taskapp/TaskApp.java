9207feb15c687d0e8e123988376894efc796c041
hirokosakoda <rrskdhrk1110@gmail.com>
2018-08-06 07:45:58 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/hiroko/sakoda/taskapp/TaskApp.java
@@ -0,0 +1,13 @@
+package jp.techacademy.hiroko.sakoda.taskapp;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class TaskApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
